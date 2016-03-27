08b9c8f3ba0cec3ed386c1d1b6792e6b3a7f4400
git-onepixel <onepixel@126.com>
2016-03-27 23:48:56 +0800

    add file

--- http/HttpServer.js
+++ http/HttpServer.js
@@ -126,7 +126,7 @@ module.exports = (function(){
         ///配置信息
         config:{
             port:8888,
-            ip:'onepixel.top',
+            ip:'127.0.0.1',
             mime:{
                 html:"text/html",
                 js:"text/javascript",
